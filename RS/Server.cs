d5a3a78ac7ec3e824cc5723a432039a46c4ec36d
VladimirShatrov <vova.shatrov.2005@gmail.com>
2025-03-18 11:29:55 +0500

    fix

--- RS/Server.cs
+++ RS/Server.cs
@@ -181,7 +181,7 @@ namespace AppTcpServer
 
             try
             {
-                listener = new TcpListener(IPAddress.Parse("127.0.0.1"), port);
+                listener = new TcpListener(IPAddress.Parse("0.0.0.0"), port);
                 listener.Start();
 
                 Console.WriteLine("Ожидание подключений...");
